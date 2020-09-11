d6d9c2e45e56bdb7eff8c3daf58a1e43cb3bfe28
GAMELEIRA <gameleira270499@gmail.com>
2020-09-11 20:44:51 -0300

    Adicionado em AULA 07\RESOLUCAO EXERCICIOS\NIVEL-01\EXERCICIO-01o projeto EXERCICIO-02 que têm como objetivo criar uma lista e inserir 10 números inteiros e apresentar o inverso dele

---
+++ AULA 07/RESOLUCAO EXERCICIOS/NIVEL-01/EXERCICIO-02/src/Application.kt
@@ -0,0 +1,33 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 11/09/2020
+ * HORA: 20:00
+ * FINALIDADE DO PROGRAMA: CRIAR UM VETOR QUE RECEBA 5 NUMEROS INTEIROS
+ * */
+
+fun main(){
+
+    val numeros = arrayListOf<Int>();
+
+    inserirNumeros(numeros);
+
+    println(numeros.reversed());
+
+}
+
+fun inserirNumeros(numeros: ArrayList<Int>) {
+
+    var numero: Int = 0;
+
+    for(i in 1..2) {
+
+        println("Informe o número $i: ");
+
+        numero = readLine()!!.toInt();
+
+        numeros.add(numero);
+
+    }
+
+}
+
