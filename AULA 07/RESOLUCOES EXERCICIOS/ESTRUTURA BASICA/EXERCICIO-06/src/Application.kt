974d107a0cebb67065af7a007ed000fd4e0c2598
GAMELEIRA <gameleira270499@gmail.com>
2020-09-14 17:30:50 -0300

    Adicionado em \AULA 07\RESOLUCOES EXERCICIOS\ESTRUTURA BASICA\EXERCICIO-06 o projeto EXERCICIO-06 que têm como objetivo imprimir os 100 primeiros números positivos

---
+++ AULA 07/RESOLUCOES EXERCICIOS/ESTRUTURA BASICA/EXERCICIO-06/src/Application.kt
@@ -0,0 +1,22 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 14/09/2020
+ * HORA: 17:26
+ * FINALIDADE DO PROGRAMA: ESCREVER UM PROGRAMA QUE EXIBA OS 100 PRIMEIROS NUMEROS POSITIVOS
+ * */
+
+fun main () {
+
+    imprimirCemNumerosPositivos();
+
+}
+
+fun imprimirCemNumerosPositivos() {
+
+    for(i in 1..100) {
+
+        println(i);
+
+    }
+
+}
