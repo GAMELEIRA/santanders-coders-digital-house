1eb0714cd291986ad86a1f2dcc226f1e4c36a2e8
GAMELEIRA <gameleira270499@gmail.com>
2020-09-14 17:02:37 -0300

    Adicionado em \AULA 07\RESOLUCOES EXERCICIOS\ESTRUTURA BASICA\EXERCICIO-04 o projeto EXERCICIO-04 que têm como objetivo imprimir os 100 primeiros numeros impares positivos

---
+++ AULA 07/RESOLUCOES EXERCICIOS/ESTRUTURA BASICA/EXERCICIO-04/src/Application.kt
@@ -0,0 +1,24 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 14/09/2020
+ * HORA: 16:38
+ * FINALIDADE DO PROGRAMA: IMPRIMIR NA TELA OS 100 PRIMEIROS NUMEROS IMPARES
+
+ * */
+
+fun main() {
+
+    imprimirCemNumerosInteirosPositivosImpares();
+
+}
+
+fun imprimirCemNumerosInteirosPositivosImpares() {
+
+    for(i in 1..200 step 2 ){
+
+        println(i);
+
+    }
+
+
+}
