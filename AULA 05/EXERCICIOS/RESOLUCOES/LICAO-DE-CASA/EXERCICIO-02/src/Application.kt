89ad6f7d7d7817d6b38b3933aa6f00b9df0895d7
GAMELEIRA <gameleira270499@gmail.com>
2020-09-14 18:18:23 -0300

    Adicionado em AULA 05\EXERCICIOS\RESOLUCOES\LICAO-DE-CASA\EXERCICIO-02 o projeto EXERCICIO-02 que têm como objetivo calcular o salário de uma pessoa.

---
+++ AULA 05/EXERCICIOS/RESOLUCOES/LICAO-DE-CASA/EXERCICIO-02/src/Application.kt
@@ -0,0 +1,36 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 14/09/2020
+ * HORA: 18:09
+ * FINALIDADE DO PROGRAMA:
+ * */
+
+fun main() {
+
+    solicitarInformacoes();
+
+}
+
+fun solicitarInformacoes() {
+
+    var quantoGanhaPorHora: Double = 0.0;
+
+    var quantidadeHorasTrabalhadas: Double = 0.0;
+
+    println("Quanto você ganha por hora?");
+
+    quantoGanhaPorHora = readLine()!!.toDouble();
+
+    println("Quantas horas você trabalha?");
+
+    quantidadeHorasTrabalhadas = readLine()!!.toDouble();
+
+    calcularSalario(quantidadeHorasTrabalhadas, quantoGanhaPorHora);
+
+}
+
+fun calcularSalario(quantidadeHorasTrabalhadas: Double, quantoGanhaPorHora: Double) {
+
+    println("Você ganha: " + (quantidadeHorasTrabalhadas * quantoGanhaPorHora) + " reais.");
+
+}
