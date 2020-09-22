1fad4e022bc950e4523423915cd692126a637af6
GAMELEIRA <gameleira270499@gmail.com>
2020-09-21 21:02:24 -0300

    Adicionando em \AULA-11\EXERCICIOS\RESOLUCOES\EXERCICIO-02 o projeto EXERCICIO-02 que têm como objetivo comparar o tamanho das cocas

---
+++ AULA-11/EXERCICIOS/RESOLUCOES/EXERCICIO-02/src/main/Application.kt
@@ -0,0 +1,20 @@
+package main
+
+import models.Coca
+
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 21/09/2020
+ * HORA: 20:57
+ * FINALIDADE DO PROGRAMA: COMPARAR O TAMANHO DAS COCAS
+ * */
+
+fun main(){
+
+    val coca1 = Coca(5, 5.0);
+
+    val coca2 = Coca(0, 58.0);
+
+    println(coca1 == coca2);
+
+}
